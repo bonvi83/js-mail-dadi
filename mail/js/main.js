6cfc11501a9251f5195a62d281adc8243ee63e9c
bonvi83 <bonvi83@gmail.com>
2024-01-12 22:06:04 +0100

    add struttura js

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,2 +1,11 @@
+// chiedo indirizzo email
 let emailUtente = prompt("Scrivi il tuo indirizzo email")
+console.log(emailUtente)
+
+
+// creo una lista di email
+const lista = ['tiziano.gattaro@boolean.com','gigi.nazionale@boolean.com','mattia.pizzettato@boolean.com' , `classe117@boolean.com`]
+
+
+// la risposta
 let risposta = document.getElementById("risposta")
