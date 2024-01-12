9ef629309ec7b54f52258d4cf6a92ce98bf924ef
bonvi83 <bonvi83@gmail.com>
2024-01-12 16:05:29 +0100

    i dadi li ho portati a casa!!!!!!!!!!

--- gioco dei dadi/js/main.js
+++ gioco dei dadi/js/main.js
@@ -12,15 +12,12 @@ bottone.addEventListener("click",
         var numeroPc = Math.floor(Math.random () * 6 + 1);
         console.log(numeroPc);
 
-        if(numeroUtente > numeroPc) {
-            
+        if (numeroUtente > numeroPc) {
+            risultato.innerHTML = `Hai VINTO, il tuo numero è ${numeroUtente}, il numero del pc è ${numeroPc}`
+        } else if  (numeroUtente < numeroPc) {
+            risultato.innerHTML = `Hai PERSO, il tuo numero è ${numeroUtente}, il numero del pc è ${numeroPc}`
+        } else {
+            risultato.innerHTML = `PAREGGIO, il tuo numero è ${numeroUtente}, il numero del pc è ${numeroPc}`
         }
-
-
-
-
-
-
     }
-
 )
