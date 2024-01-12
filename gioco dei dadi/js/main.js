368f2ece7e3fff79c9c19107e88190ee8aba69e5
bonvi83 <bonvi83@gmail.com>
2024-01-12 15:54:06 +0100

    forse con js ci siamo

---
+++ gioco dei dadi/js/main.js
@@ -0,0 +1,26 @@
+let risultato = document.getElementById("risultato");
+
+let bottone = document.getElementById("bottone");
+
+
+// funzione per generare numeri casuali
+bottone.addEventListener("click",
+    function () {
+        var numeroUtente = Math.floor(Math.random () * 6 + 1);
+        console.log(numeroUtente);
+
+        var numeroPc = Math.floor(Math.random () * 6 + 1);
+        console.log(numeroPc);
+
+        if(numeroUtente > numeroPc) {
+            
+        }
+
+
+
+
+
+
+    }
+
+)
