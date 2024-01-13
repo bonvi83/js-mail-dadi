3dc34be0e45f80cc1186a6f88cb9f1ff2924c1c0
bonvi83 <bonvi83@gmail.com>
2024-01-13 09:37:23 +0100

    ovviamente non funziona...

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,11 +1,22 @@
-// chiedo indirizzo email
-let emailUtente = prompt("Scrivi il tuo indirizzo email")
+// chiedo indirizzo email all'utente
+let emailUtente = prompt("Scrivi il tuo indirizzo e-mail");
 console.log(emailUtente)
 
 
-// creo una lista di email
-const lista = ['tiziano.gattaro@boolean.com','gigi.nazionale@boolean.com','mattia.pizzettato@boolean.com' , `classe117@boolean.com`]
+// lista di email che libero accesso
+const lista = ["tiziano.gattaro@boolean.com", "gigi.nazionale@boolean.com", "mattia.pizzettato@boolean.com", "classe117@boolean.com"];
 
 
-// la risposta
-let risposta = document.getElementById("risposta")
+// stampo la risposta
+let risposta = document.getElementById("risposta");
+
+
+for (let i=0, i < lista.length; i++) {
+    console.log(lista[i
+    if (lista[i] == emailUtente) {
+        risposta.innerHTML = "Accesso CONSENTITO";
+    }
+    else {
+        risposta.innerHTML = "Accesso NEGATO";
+    }
+}
