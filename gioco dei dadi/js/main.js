54370bc9934a15975d0404760d40a3d9cdcc6e31
bonvi83 <bonvi83@gmail.com>
2024-01-15 15:07:11 +0100

    commit

--- gioco dei dadi/js/main.js
+++ gioco dei dadi/js/main.js
@@ -6,10 +6,10 @@ let bottone = document.getElementById("bottone");
 // funzione per generare numeri casuali
 bottone.addEventListener("click",
     function () {
-        var numeroUtente = Math.floor(Math.random () * 6 + 1);
+        let numeroUtente = Math.floor(Math.random () * 6 + 1);
         console.log(numeroUtente);
 
-        var numeroPc = Math.floor(Math.random () * 6 + 1);
+        let numeroPc = Math.floor(Math.random () * 6 + 1);
         console.log(numeroPc);
 
         if (numeroUtente > numeroPc) {
